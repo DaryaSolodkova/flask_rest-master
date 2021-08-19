73b5d7a78bdd91603df80a352e7f4124dfff7ba7
DaryaSolodkova <dashasolodkova@gmail.com>
2021-08-19 12:18:43 +0300

    Initial commit

---
+++ bh_flask_rest-master/db/__init__.py
@@ -0,0 +1,9 @@
+from .session import Session, session_scope
+from . import tables
+
+
+__all__ = (
+    'Session',
+    'session_scope',
+    'tables'
+)
---
+++ bh_flask_rest-master/views/__init__.py
@@ -0,0 +1,6 @@
+from .app import app
+# add you routes here
+from .film import *
+from .actor import *
+from .country import *
+from .genre import *
